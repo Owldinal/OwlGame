1942f806175cfe0702faa25bb4161e6bf8fdaa23
owl <owlmerlinchain@gmail.com>
2024-04-12 17:33:36 +0800

    implement more script

---
+++ contract/scripts/ganerate-owltoken-for-test.js
@@ -0,0 +1,135 @@
+const hre = require("hardhat");
+const { deploy } = require('./utils');
+
+let deployer, ownerAddress;
+
+// deploy contracts
+let owlTokenContract, owlTokenAddress;
+let owldinalNftContract, owldinalNftAddress;
+let owlGameContract, owlGameAddress;
+let genOneBoxContract, genOneBoxAddress;
+
+const decimal = BigInt(10) ** BigInt(18)
+
+async function main() {
+	[deployer] = await hre.ethers.getSigners();
+	ownerAddress = deployer.address;
+	backendAddress = process.env.BACKEND_WALLET;
+	let tx;
+	console.log(`Address = ${deployer.address}`);
+
+	const blockNumber = await hre.ethers.provider.getBlockNumber();
+	console.log(`Current Block number: ${blockNumber}`);
+
+	owlTokenAddress = "0xF86EA7e636f43dd6dCD35B7E04742f5508F9CF73";
+	owldinalNftAddress = "0x96fB752fc565c740F8b09a1760FE5a3D89dD18E5";
+	owlGameAddress = "0xC0E5d058eeF687B0c3cEf2967D0B55AD81eb9C21";
+	genOneBoxAddress = "0xC9761572c264ADE5253d4F56a574fa0F4905ca5d";
+
+	await deployOrConnect();
+
+	console.log(`
+owlTokenAddress= "${owlTokenAddress}";
+owldinalNftAddress= "${owldinalNftAddress}";
+owlGameAddress= "${owlGameAddress}";
+genOneBoxAddress= "${genOneBoxAddress}";
+		`);
+
+	console.log(`
+EVENT_START_BLOCK=${blockNumber}
+NFT_OWL_ADDR=${owldinalNftAddress}
+OWL_TOKEN_ADDR=${owlTokenAddress}
+NFT_MYSTERY_BOX_ADDR=${genOneBoxAddress}
+OWL_GAME_ADDR=${owlGameAddress}
+			`);
+
+	const receiver = "0xB2eC5D8Fd46089172dD394efe0fE5553f3fBB49e";
+	for (i = 20; i < 170; i++) {
+		console.log('transfer nft ', i);
+		await owldinalNftContract.connect(deployer).mintByAdmin();
+		await owldinalNftContract.connect(deployer).transferFrom(deployer.address, receiver, i);
+	}
+	// for (i = 20; i < 170; i++) {
+	// 	await owldinalNftContract.connect(deployer).transferFrom(deployer.address, receiver, i);
+	// }
+
+	await owlTokenContract.connect(deployer).mint(receiver, BigInt(100000 * 100000) * decimal);
+}
+
+async function printTxDetail(tx, msg) {
+	const receipt = await tx.wait();
+	const gasUsed = receipt.gasUsed;
+	console.log(`${msg} [Gas=${gasUsed}, DataLen=${tx.data.length}]`);
+}
+
+async function deployOrConnect() {
+	if (owlTokenAddress) {
+		owlTokenContract = await hre.ethers.getContractFactory("OwlToken").then((c) => c.attach(owlTokenAddress));
+		console.log(`OwlToken contract connected to : ${owlTokenAddress}`);
+	} else {
+		const params = [ownerAddress];
+		[owlTokenContract, owlTokenAddress] = await deploy("OwlToken", params);
+		console.log(`OwlToken contract deployed to : ${owlTokenAddress}\nParams = ${params.join(" ")}`);
+
+		await owlTokenContract.connect(deployer).mint(deployer.address, BigInt(100000000) * decimal);
+	}
+
+	if (owldinalNftAddress) {
+		owldinalNftContract = await hre.ethers.getContractFactory("Owldinal").then((c) => c.attach(owldinalNftAddress));
+		console.log(`Owldinal contract connected to : ${owldinalNftAddress}`);
+	} else {
+		const params = [10000000, owlTokenAddress, deployer.address, backendAddress];
+		[owldinalNftContract, owldinalNftAddress] = await deploy("Owldinal", params);
+		console.log(`Owldinal contract deployed to : ${owldinalNftAddress}\nParams = ${params.join(" ")}`);
+
+		await owldinalNftContract.connect(deployer).mintByAdmin();
+		await owldinalNftContract.connect(deployer).mintByAdmin();
+		await owldinalNftContract.connect(deployer).mintByAdmin();
+	}
+
+	if (owlGameAddress) {
+		owlGameContract = await hre.ethers.getContractFactory("OwlGame").then((c) => c.attach(owlGameAddress));
+		console.log(`OwlGame contract connected to : ${owlGameAddress}`);
+	} else {
+		const params = [ownerAddress];
+		[owlGameContract, owlGameAddress] = await deploy("OwlGame", params);
+		console.log(`OwlGame contract deployed to : ${owlGameAddress}\nParams = ${params.join(" ")}`);
+	}
+
+	if (genOneBoxAddress) {
+		genOneBoxContract = await hre.ethers.getContractFactory("OwldinalGenOneBox").then((c) => c.attach(genOneBoxAddress));
+		console.log(`OwldinalGenOneBox contract connected to : ${genOneBoxAddress}`);
+	} else {
+		const params = [owlTokenAddress, owlGameAddress];
+		[genOneBoxContract, genOneBoxAddress] = await deploy("OwldinalGenOneBox", params);
+		console.log(`OwldinalGenOneBox contract deployed to : ${genOneBoxAddress}\nParams = ${params.join(" ")}`);
+	}
+
+}
+
+function decodeInviteCode(encoded) {
+	let inviteCode = '';
+	for (let i = 0; i < 5; i++) {
+		const charValue = Number((encoded >> (BigInt(i) * 5n)) & 0x1Fn);
+		inviteCode += String.fromCharCode(charValue + 0x41);
+	}
+	return inviteCode;
+}
+
+function encodeInviteCode(inviteCode) {
+	if (inviteCode.length !== 5) {
+		throw new Error("Invalid invite code length");
+	}
+	let encoded = 0;
+	for (let i = 0; i < 5; i++) {
+		let char = inviteCode[i];
+		let charValue = char.charCodeAt(0) - 0x41;
+		encoded |= (charValue << (i * 5));
+	}
+	return encoded;
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
---
+++ contract/scripts/update-fruit-rewards.js
@@ -0,0 +1,56 @@
+const hre = require("hardhat");
+const { deploy } = require('./utils');
+
+let deployer, ownerAddress;
+
+// deploy contracts
+let owlGameContract, owlGameAddress;
+
+const decimal = BigInt(10) ** BigInt(18)
+
+async function main() {
+	[deployer] = await hre.ethers.getSigners();
+	ownerAddress = deployer.address;
+	let tx;
+
+	owlGameAddress = "0x49fd2BE640DB2910c2fAb69bB8531Ab6E76127ff";
+	owlGameContract = await hre.ethers.getContractFactory("OwlGame").then((c) => c.attach(owlGameAddress));
+	console.log(`OwlGame contract connected to : ${owlGameAddress}`);
+
+	console.log(`[${new Date().toLocaleString()}] Start updateAllFruitRewards`);
+	tx = await owlGameContract.connect(deployer).updateAllFruitRewards({ gasLimit: 100_000_000_000 });
+	await printTxDetail(tx, 'updateAllFruitRewards');
+}
+
+async function printTxDetail(tx, msg) {
+	const receipt = await tx.wait();
+	const gasUsed = receipt.gasUsed;
+	console.log(`[${new Date().toLocaleString()}] ${msg} [Gas=${gasUsed}, DataLen=${tx.data.length}]`);
+}
+
+function decodeInviteCode(encoded) {
+	let inviteCode = '';
+	for (let i = 0; i < 5; i++) {
+		const charValue = Number((encoded >> (BigInt(i) * 5n)) & 0x1Fn);
+		inviteCode += String.fromCharCode(charValue + 0x41);
+	}
+	return inviteCode;
+}
+
+function encodeInviteCode(inviteCode) {
+	if (inviteCode.length !== 5) {
+		throw new Error("Invalid invite code length");
+	}
+	let encoded = 0;
+	for (let i = 0; i < 5; i++) {
+		let char = inviteCode[i];
+		let charValue = char.charCodeAt(0) - 0x41;
+		encoded |= (charValue << (i * 5));
+	}
+	return encoded;
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
