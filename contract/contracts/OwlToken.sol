595a51ade305c1be345395530597c386dd1dcbbe
owl <owlmerlinchain@gmail.com>
2024-04-12 16:18:32 +0800

    only admin can mint test owl_token

--- contract/contracts/OwlToken.sol
+++ contract/contracts/OwlToken.sol
@@ -6,13 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract OwlToken is ERC20, Ownable, ERC20Permit, ERC20Burnable {
+contract OwlToken is ERC20, Ownable, ERC20Permit, ERC20Burnable, AccessControl {
     constructor(
         address initialOwner
-    ) ERC20("OwlToken", "Owl") Ownable(initialOwner) ERC20Permit("OwlToken") {}
+    ) ERC20("OwlToken", "Owl") Ownable(initialOwner) ERC20Permit("OwlToken") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    function mint(address to, uint256 amount) public {
+    function mint(
+        address to,
+        uint256 amount
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         _mint(to, amount);
     }
 }
